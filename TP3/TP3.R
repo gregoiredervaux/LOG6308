72128e4ad5376dfb1f7a68a0309e5e3fab666b42
gregoire <gregoire.dervaux@2019.icam.fr>
2019-11-12 12:18:51 -0500

    matrice tot

--- TP3/TP3.R
+++ TP3/TP3.R
@@ -15,6 +15,8 @@ corpus_udm <- get_data('data/UdM')
 corpus_hec <- get_data('data/HEC')
 corpus_uqam <- get_data('data/UQAM')
 
+corpus_tot <- get_data(c('data/Poly', 'data/UdM', 'data/HEC', 'data/UQAM'))
+
 to_matrix <- function(corpus) {
   corpus <- tm_map(corpus,removeWords, stopwords('french'))
   corpus <- tm_map(corpus, removeWords, c('titrecours', 'descriptioncours'))
@@ -25,4 +27,10 @@ td_poly <- to_matrix(corpus_poly)
 td_udm <- to_matrix(corpus_udm)
 td_hec <- to_matrix(corpus_hec)
 td_uqam <- to_matrix(corpus_uqam)
+
+td_tot <- to_matrix(corpus_tot)
+
+get_Tf-IDF <- function() {
+  return(FALSE)
+}
 
