b5da89eb72d3d13d1491f21ff8d19990a854217d
gregoire <gregoire.dervaux@2019.icam.fr>
2019-11-12 11:45:10 -0500

    term document matrix

--- TP3/TP3.R
+++ TP3/TP3.R
@@ -1,5 +1,6 @@
 library(NLP)
 library(tm)
+library(ggplot2)
 
 get_data <- function(src) {
   corpus <- SimpleCorpus(DirSource(src))
@@ -14,4 +15,14 @@ corpus_udm <- get_data('data/UdM')
 corpus_hec <- get_data('data/HEC')
 corpus_uqam <- get_data('data/UQAM')
 
-                      
+to_matrix <- function(corpus) {
+  corpus <- tm_map(corpus,removeWords, stopwords('french'))
+  corpus <- tm_map(corpus, removeWords, c('titrecours', 'descriptioncours'))
+  return(TermDocumentMatrix(corpus))
+}
+
+td_poly <- to_matrix(corpus_poly)
+td_udm <- to_matrix(corpus_udm)
+td_hec <- to_matrix(corpus_hec)
+td_uqam <- to_matrix(corpus_uqam)
+
