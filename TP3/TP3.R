c0d2b3cec1a6f88195c33cf8bf5bfe2985285519
gregoire <gregoire.dervaux@2019.icam.fr>
2019-11-12 18:59:53 -0500

    question 1 et debut question 2

--- TP3/TP3.R
+++ TP3/TP3.R
@@ -1,7 +1,8 @@
+library(Matrix)
 library(NLP)
 library(tm)
-library(ggplot2)
-library(Matrix)
+library(irlba)
+library(stats)
 
 get_data <- function(src) {
   corpus <- SimpleCorpus(DirSource(src))
@@ -42,14 +43,89 @@ get_Tf_IDF <- function(td) {
 }
 
 # tf_idf <- get_Tf_IDF(td_tot)
-tf_idf_dev <- get_Tf_IDF(td_tot[1:1000, 1:750])
+tf_idf <- Matrix(get_Tf_IDF(td_tot), "dgCMatrix")
+head(tf_idf[, 0:5])
 
 dim_redu = 50
 
-m.svd <- svd(t(tf_idf_dev), nu = nrow(tf_idf_dev), nv = 50)
+m.svd <- irlba(t(tf_idf), 50)
 reduce_tf_idf <- m.svd$u[, 1:dim_redu] %*% diag(m.svd$d[1:dim_redu])
-rownames(reduce_tf_idf) <- colnames(tf_idf_dev)
-head(reduce_tf_idf[, 1:7])
+rownames(reduce_tf_idf) <- colnames(tf_idf)
+head(reduce_tf_idf[, 1:5])
+
+# Question 1
+
+## Cosinus entre un vecteur v et chaque colonne dela matrice m
+cosinus.vm <- function(v,m) { n <- sqrt(colSums(m^2)); (v %*% m)/(n * sqrt(sum(v^2))) }
+## Cosinus des colonnes d'une matrice
+cosinus.mm <- function(m) { n <- sqrt(colSums(m^2)); crossprod(m)/(n %o% n) }
+# Trouve les indexes des premières 'n' valeurs maximales d'une matrice
+max.nindex <- function(m, n=5) {
+  i <- order(m, decreasing=TRUE)
+  return(i[1:n])
+}
+min.nindex <- function(m, n=5) {
+  i <- order(m)
+  return(i[1:n])
+}
+
+i <- grep('LOG2420', as.character(rownames(reduce_tf_idf)))
+reduce_tf_idf.cos <- cosinus.mm(t(reduce_tf_idf))
+i.sim.cos <- max.nindex(reduce_tf_idf.cos[,i], 11)
+data.frame(Cos=reduce_tf_idf.cos[i.sim.cos, i], Index=i.sim.cos)
 
 
+reduce_tf_idf.class <- subset(reduce_tf_idf, grepl("PSY|PHY", rownames(reduce_tf_idf)))
 
+reduce_tf_idf.PSY <- subset(reduce_tf_idf, grepl("PSY", rownames(reduce_tf_idf)))
+
+reduce_tf_idf.PHY <- subset(reduce_tf_idf, grepl("PHY", rownames(reduce_tf_idf)))
+
+
+nfolds = 10
+
+split <- rep(1:nfolds, length.out=nrow(reduce_tf_idf.PSY))[sample(nrow(reduce_tf_idf.PSY))]
+reduce_tf_idf.PSY.test.i <- (split==nfolds)
+reduce_tf_idf.PSY.train.i <- !reduce_tf_idf.PSY.test.i
+
+split <- rep(1:nfolds, length.out=nrow(reduce_tf_idf.PHY))[sample(nrow(reduce_tf_idf.PHY))]
+reduce_tf_idf.PHY.test.i <- (split==nfolds)
+reduce_tf_idf.PHY.train.i <- !reduce_tf_idf.PHY.test.i
+
+reduce_tf_idf.PSY.train <- reduce_tf_idf.PSY[reduce_tf_idf.PSY.train.i,]
+reduce_tf_idf.PHY.train <- reduce_tf_idf.PHY[reduce_tf_idf.PHY.train.i,]
+
+centroide.PSY <- colMeans(reduce_tf_idf.PSY[reduce_tf_idf.PSY.train.i,])
+centroide.PHY <- colMeans(reduce_tf_idf.PHY[reduce_tf_idf.PHY.train.i,])
+
+
+reduce_tf_idf.PSY.test <- reduce_tf_idf.PSY[reduce_tf_idf.PSY.test.i,]
+reduce_tf_idf.PHY.test <- reduce_tf_idf.PHY[reduce_tf_idf.PHY.test.i,]
+
+cours <- rownames(reduce_tf_idf.PSY.test)
+sim.PSY <- cosinus.vm(centroide.PSY, t(reduce_tf_idf.PSY.test))[1,]
+sim.PHY <- cosinus.vm(centroide.PHY, t(reduce_tf_idf.PSY.test))[1,]
+
+prediction.PSY <- data.frame(cours = rownames(reduce_tf_idf.PSY.test),
+                             sim.PSY = as.character(cosinus.vm(centroide.PSY, t(reduce_tf_idf.PSY.test))),
+                             sim.PHY = as.character(cosinus.vm(centroide.PHY, t(reduce_tf_idf.PSY.test))))
+
+prediction.PSY$pred.PSY <- apply(prediction.PSY, 1, function (v) {v['sim.PSY'] > v['sim.PHY']})
+prediction.PSY$pred.PHY <- apply(prediction.PSY, 1, function (v) {v['sim.PHY'] > v['sim.PSY']})
+prediction.PSY$is.PSY <- TRUE
+prediction.PSY$is.PHY <- FALSE
+
+
+prediction.PHY <- data.frame(cours = rownames(reduce_tf_idf.PHY.test),
+                             sim.PSY = as.character(cosinus.vm(centroide.PSY, t(reduce_tf_idf.PHY.test))),
+                             sim.PHY = as.character(cosinus.vm(centroide.PHY, t(reduce_tf_idf.PHY.test))))
+
+prediction.PHY$pred.PHY <- apply(prediction.PHY, 1, function (v) {v['sim.PHY'] > v['sim.PSY']})
+prediction.PHY$pred.PSY <- apply(prediction.PHY, 1, function (v) {v['sim.PSY'] > v['sim.PHY']})
+prediction.PHY$is.PHY <- TRUE
+prediction.PHY$is.PSY <- FALSE
+
+head(prediction.PHY)
+
+prediction.class <- rbind(prediction.PHY, prediction.PSY)
+responce.PSY <- roc(prediction.PSY$is.PSY, !vector(,nrow(prediction.PSY)))
